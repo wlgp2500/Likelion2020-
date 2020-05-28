7a409bb9dd86b5a209fbeff1259c7c74b17fb612
wlgp2500 <wlgp2500@naver.com>
2020-05-28 14:02:38 +0900

    complete

---
+++ Project/theapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(requests):
+    return render(requests, 'main.html')
+def intro(requests):
+    return render(requests,'intro.html')
+def photos(requests):
+    return render(requests,'photos.html')
+def notices(requests):
+    return render(requests,'notices.html')
