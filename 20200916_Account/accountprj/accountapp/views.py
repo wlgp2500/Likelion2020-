72a539a9a0ff09578487cfebaec0849b8bbc1b74
NewWisdom <wlgp2500@gmail.com>
2020-09-17 11:25:56 +0900

    회원 가입 & 소셜 로그인 & 로그아웃 구현

---
+++ 20200916_Account/accountprj/accountapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+# Create your views here.
+def main(request):
+    return render(request,'main.html')
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'],
+                password = request.POST['password1']
+            )
+            auth.login(request,user)
+            return redirect('main')
+    return render(request,'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username,password = password)
+        if user is not None:
+            auth.login(request,user)
+        else:
+            return render(request,'login.html')
+    return render(request,'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('main')
+    return render(request,'signup.html')
